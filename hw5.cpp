c31dbeb5a6e7208c3cc3003e2e5d77900f695918
Shannon Hull <113312870+hullsg@users.noreply.github.com>
2022-11-14 22:39:33 -0500

    Add files via upload

---
+++ hw5.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdint>
+using std::cout;
+using std::cin;
+using std::endl;
+int main(){
+
+    while (true){
+
+        int x;
+        cout << "Type a number: "; // Type a number and press enter
+        cin >> x; // Get user input from the keyboard
+
+        if (x == 0){
+            break;
+        }
+        else{
+
+    
+    
+        int halfn=x/2;
+        int arr[100] = {0};
+        int total=0;
+        cout<<x<<":";
+        for (int i = 1; i <= halfn; i++) {
+            int remainder = (x%i);
+            if (remainder==0){
+                total+=i;
+                int tmp = i;
+            //cout<<tmp;
+        cout<<tmp<<"+";    
+        //cout<<x<<":"<<tmp; 
+                }   
+        
+        }
+        cout<<"="<<total;
+        //return 0;  //need to exit program when 0 is entered
+        }
+    }
+}
