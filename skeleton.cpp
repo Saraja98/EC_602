e1e3245ec9f4f74c8bdcfb8b5488bce6c3509e52
Saraja98 <90469291+Saraja98@users.noreply.github.com>
2022-12-07 14:22:55 -0500

    Create skeleton.cpp

---
+++ skeleton.cpp
@@ -0,0 +1,22 @@
+//Copyright 2022 Shannon Hull sghull@bu.edu
+//Copyright 2022 Saraja saraja@bu.edu
+
+#include <iostream>
+#include <string>
+#include <vector>
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+
+
+int main()
+{   
+
+}
+    
+   
+
+   
