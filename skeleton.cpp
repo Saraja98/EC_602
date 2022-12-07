4ac525db27dea282d10a32162a3d3546ca404e24
Shannon Hull <113312870+hullsg@users.noreply.github.com>
2022-12-07 15:42:25 -0500

    Update skeleton.cpp

--- skeleton.cpp
+++ skeleton.cpp
@@ -18,7 +18,7 @@ void heat(const vector<int> &vect){
         if(i%2!=0)
            cout << "Time"<<vect[i] << " ";
         else
-           cout <<"Player"<<vect[i] << " ";
+           cout <<"Runner"<<vect[i] << " ";
             
         
     
@@ -33,7 +33,23 @@ int main() {
    //int a;
    //vector<int> v;
    // user can add 16 element,(player,time)
-   vector <int> original = {1,2,3,4,5,6,7,8,10,11,12,13,14,15,16}; // creating the original vector
+   //vector <int> original = {1,2,3,4,5,6,7,8,10,11,12,13,14,15,16}; // creating the original vector
+
+   //heat( original ); // calling the function func
+   
+   // user can input runner and time in pairs, with a space in between)
+    vector <int> original;
+    cout<<"Input runner number, then time, in pairs"<<endl;
+   int runner;
+   int speed;
+  int n=8;
+   for (int i=0; i<n; i++){
+   cin>>runner>>speed;
+    original.push_back(runner);
+    original.push_back(speed);
+   }
+ 
+
 
    heat( original ); // calling the function func
 
