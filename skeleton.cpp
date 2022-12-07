67c6b9f3294d57d993f6f76d73b75d1aaf01ea5c
Saraja98 <90469291+Saraja98@users.noreply.github.com>
2022-12-07 15:15:35 -0500

    Update skeleton.cpp

--- skeleton.cpp
+++ skeleton.cpp
@@ -8,15 +8,42 @@ using std::string;
 using std::cout;
 using std::cin;
 using std::endl;
+using std::vector;
 
 
-
-
-int main()
-{   
-
-}
+//vector<int> myVector;
+void heat(const vector<int> &vect){
     
+    for (int i = 0; i < vect.size(); i++) {
+        if(i%2!=0)
+           cout << "Time"<<vect[i] << " ";
+        else
+           cout <<"Player"<<vect[i] << " ";
+            
+        
+    
+    }
+        
+    
+    
+    }
+
+
+int main() {
+   //int a;
+   //vector<int> v;
+   // user can add 16 element,(player,time)
+   vector <int> original = {1,2,3,4,5,6,7,8,10,11,12,13,14,15,16}; // creating the original vector
+
+   heat( original ); // calling the function func
+
+  // heat("12345678")
+
+   //heat(v);
+   
+
+   return 0;
+} 
    
 
    
